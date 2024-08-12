802f6e6dffede00594d2c627f41587ba0c5bd46e
Thomas King <thomasgking42@gmail.com>
2024-08-12 16:16:56 +0100

    story3 task 2

--- story3.js
+++ story3.js
@@ -11,10 +11,10 @@ fetch('./films.json')
                 array[j] = temp;
             }
         }
-
+//shuffling film object
         shuffleArray(films)
         console.log(films)
-
+//Getting quote
         const currentQuote = films.pop()
         console.log(currentQuote.quote)
 
@@ -23,6 +23,32 @@ fetch('./films.json')
         const quoteElem = document.createElement('p')
         quoteElem.textContent = "Quote: " + currentQuote.quote
         quoteContainer.appendChild(quoteElem)
+
+
+        //Getting titles
+
+
+        const wrongAnswer1 = films[0].title
+        const wrongAnswer2 = films[1].title
+        const correctAnswer = currentQuote.title
+        const answers = [wrongAnswer1, wrongAnswer2, correctAnswer]
+        shuffleArray(answers)
+
+
+        const titleContainer = document.querySelector('.titleContainer')
+        const button1 = document.createElement('button')
+        const button2 = document.createElement('button')
+
+        const button3 = document.createElement('button')
+        button1.textContent = answers[0]
+        button2.textContent = answers[1]
+        button3.textContent = answers[2]
+        console.log(answers)
+        titleContainer.appendChild(button1)
+
+        titleContainer.appendChild(button2)
+
+        titleContainer.appendChild(button3)
     })
 
 
