8a5a16ee15f51eff7ad5a7403b83a50107c102ba
Henry Tottle <henrywilliamtottle@gmail.com>
2024-08-13 09:20:08 +0100

    cleaning up check answer function

--- story3.js
+++ story3.js
@@ -27,10 +27,13 @@ fetch('./films.json')
 
         //Getting titles
 
-
+        //Creating two wrong answer variables and assigning them the first two titles in the films object.
+        //Intention is to shuffle after each guess so these will hopefully not be the same each guess
         const wrongAnswer1 = films[0].title
         const wrongAnswer2 = films[1].title
         const correctAnswer = currentQuote.title
+        //putting the potential answers in an array then shuffling them so that the order they show on the page
+        //isn't always the same
         const answers = [wrongAnswer1, wrongAnswer2, correctAnswer]
         shuffleArray(answers)
 
@@ -40,30 +43,28 @@ fetch('./films.json')
         const button2 = document.createElement('button')
 
         const button3 = document.createElement('button')
+        //using the shuffled answers array to choose which order the buttons display
         button1.textContent = answers[0]
         button2.textContent = answers[1]
         button3.textContent = answers[2]
+
         console.log(answers)
+
         titleContainer.appendChild(button1)
-
         titleContainer.appendChild(button2)
-
         titleContainer.appendChild(button3)
 
-        // const checkAnswer = () => {
-        //
-        //
-        // }
-        button1.addEventListener('click',     () => {
+        const checkAnswer = () => {
             if (button1.textContent === correctAnswer) {
                 return console.log('correct')
 
             } else if (button1.textContent === wrongAnswer1 || button1.textContent === wrongAnswer2){
                 return console.log('wrong mate')
             }
-        })
-        // button2.addEventListener('click', checkAnswer)
-        // button3.addEventListener('click', checkAnswer)
+        }
+        button1.addEventListener('click', checkAnswer)
+        button2.addEventListener('click', checkAnswer)
+        button3.addEventListener('click', checkAnswer)
 
     })
 
