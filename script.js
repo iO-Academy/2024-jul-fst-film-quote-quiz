74ccf06d978324d507d32c15146acf89be677650
Henry Tottle <henrywilliamtottle@gmail.com>
2024-08-15 13:28:47 +0100

    trainer review

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
+
 const playButton = document.querySelector('.play')
 const playMenu = document.querySelector('.gameButtons')
 const button1 = document.querySelector('.btn1')
 const button2 = document.querySelector('.btn2')
 const button3 = document.querySelector('.btn3')
 const hintButton = document.querySelector('.hintButton')
+const scoreBoxElem = document.querySelector('.scoreBox')
 const quoteElem = document.querySelector('.quote')
-const buttons = document.querySelector('.buttons')
+const instructionsButton = document.querySelector('.instructionsButton')
+const instructions = document.querySelector('.modalContainer')
+const closeButton = document.querySelector('.closeButton')
+const timerDisplay = document.querySelector(".timerDisplay")
+const timerProgress = document.querySelector(".timerProgress")
+const resetBtn = document.querySelector('.reset')
+const gameOverModal = document.querySelector('.gameOverModal')
+const scoreSpan = document.querySelector('.score')
+let filmTitles = []
+let films = []
+let score = 0
+let correctAnswer
+let wrongAnswer1
+let wrongAnswer2
+let countDownValue = 30
+let countDownInterval
+
+const fetchFilms = async () => {
+    const response = await fetch('./films.json')
+    const data = await response.json()
+    return data.films
+}
+
+const setupGame = async () => {
+    filmTitles = await fetchFilms()
+    films = await fetchFilms()
+    playButton.addEventListener('click', () => playGame(filmTitles, films))
+}
+
+const modal = () => {
+    if (instructions.style.display === 'none') {
+        instructions.style.display = 'flex';
+        instructions.style.zIndex = '1';
+    } else {
+        instructions.style.display = 'none';
+    }
+}
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -15,24 +53,6 @@ function shuffleArray(array) {
         array[j] = temp;
     }
 }
-
-const fetchFilms = async () => {
-    const response = await fetch('./films.json')
-    const data = await response.json()
-    return data.films
-}
-
-
-let filmTitles = []
-let films = []
-
-const setupGame = async () => {
-    filmTitles = await fetchFilms()
-    films = await fetchFilms()
-    playButton.addEventListener('click', () => playGame(filmTitles, films))
-}
-
-setupGame()
 
 const checkAnswer = (button) => {
     button.addEventListener('click', () => {
@@ -47,50 +67,34 @@ const checkAnswer = (button) => {
                     spread: 70,
                     origin: {y: .6}
                 })
-            }
-        } else if (button.textContent === wrongAnswer1 || button.textContent === wrongAnswer2) {
+            }}
+        else if (button.textContent === wrongAnswer1 || button.textContent === wrongAnswer2) {
             button.style.backgroundColor = 'red'
         }
+
         const interval = setInterval(() => {
             button.style.backgroundColor = '#efefef'
             clearInterval(interval)
             if (films.length >= 3) {
                 playGame(filmTitles, films)
-
-
             } else {
                 endGame()
-
             }
         }, 200)
         shuffleArray(filmTitles)
     })
 }
-checkAnswer(button1)
-checkAnswer(button2)
-checkAnswer(button3)
-
-
-let score = 0
-const scoreBoxElem = document.querySelector('.scoreBox')
-let correctAnswer
-let wrongAnswer1
-let wrongAnswer2
 
 const playGame = (filmTitles, films) => {
-    console.log(films)
-
     playMenu.style.visibility = 'hidden'
     quoteElem.classList.remove('hidden')
     button1.classList.remove('hidden')
     button2.classList.remove('hidden')
     button3.classList.remove('hidden')
     hintButton.classList.remove('hidden')
-
     shuffleArray(films)
     const currentQuote = films.pop()
     quoteElem.textContent = "\"" + currentQuote.quote + "\""
-
     wrongAnswer1 = filmTitles[0].title
     wrongAnswer2 = filmTitles[1].title
     correctAnswer = currentQuote.title
@@ -101,21 +105,14 @@ const playGame = (filmTitles, films) => {
         wrongAnswer2 = filmTitles[2].title
     }
 
-
     const answers = [wrongAnswer1, wrongAnswer2, correctAnswer]
     shuffleArray(answers)
-
-    const hintContainer = document.querySelector('.hintContainer')
-
     button1.textContent = answers[0]
     button2.textContent = answers[1]
     button3.textContent = answers[2]
     hintButton.textContent = 'Hint?'
-
-    // hint
     hintButton.addEventListener('click', () => {
         hintButton.textContent = currentQuote.year
-        hintButton.disabled = true
     })
 }
 
@@ -125,34 +122,7 @@ const endGame = () => {
     gameOverModal.showModal()
     countDownValue = 30
     timerDisplay.textContent = countDownValue.toString()
-
-
 }
-
-
-const instructionsButton = document.querySelector('.instructionsButton')
-const instructions = document.querySelector('.modalContainer')
-const closeButton = document.querySelector('.closeButton')
-const modal = () => {
-    if (instructions.style.display === 'none') {
-        instructions.style.display = 'flex';
-        instructions.style.zIndex = '1';
-    } else {
-        instructions.style.display = 'none';
-    }
-}
-
-instructionsButton.addEventListener('click', modal)
-closeButton.addEventListener('click', modal)
-
-const timerDisplay = document.querySelector(".timerDisplay")
-const timerProgress = document.querySelector(".timerProgress")
-const resetBtn = document.querySelector('.reset')
-const gameOverModal = document.querySelector('.gameOverModal')
-const scoreSpan = document.querySelector('.score')
-
-let countDownValue = 30
-let countDownInterval
 
 const countDownTimer = () => {
     countDownInterval = setInterval(countDown, 1000)
@@ -168,19 +138,32 @@ const countDown = () => {
     }
 }
 
-playButton.addEventListener('click', countDownTimer)
-
-timerDisplay.textContent = countDownValue.toString()
-
 const resetGame = () => {
     score = 0
     countDownValue = 30
     scoreBoxElem.textContent = score
     setupGame()
     playGame(filmTitles, films)
-
-
 }
 
+instructionsButton.addEventListener('click', modal)
+closeButton.addEventListener('click', modal)
+playButton.addEventListener('click', countDownTimer)
+timerDisplay.textContent = countDownValue.toString()
 resetBtn.addEventListener('click', resetGame)
 resetBtn.addEventListener('click', countDownTimer)
+
+setupGame()
+checkAnswer(button1)
+checkAnswer(button2)
+checkAnswer(button3)
+
+
+
+
+
+
+
+
+
+
